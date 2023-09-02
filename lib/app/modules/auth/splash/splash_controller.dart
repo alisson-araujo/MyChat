47add266c780777524d9add1e1e5b3ed32bb381f
Alisson Araujo <alisson.ads@hotmail.com>
2023-09-02 19:25:36 -0300

    fix: correcting key of accessToken

--- lib/app/modules/auth/splash/splash_controller.dart
+++ lib/app/modules/auth/splash/splash_controller.dart
@@ -10,11 +10,12 @@ class SplashController extends GetxController {
 
   checkToken() async {
     const storage = FlutterSecureStorage();
-    final String? token = await storage.read(key: 'token');
+    final String? token = await storage.read(key: 'accessToken');
     if (token != null) {
       // TODO: check token in api
       Get.offAllNamed('/conversations');
+    } else {
+      Get.offAllNamed('/register-first-step');
     }
-    Get.offAllNamed('/register-first-step');
   }
 }
